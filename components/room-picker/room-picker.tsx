61c95a6fddb395c15f4229b59f156f9101ec75eb
Saúl Ramos <phasecross@hotmail.com>
2023-11-28 18:18:45 -0700

    Cleanup, remove react-native-dropdown

--- components/room-picker/room-picker.tsx
+++ components/room-picker/room-picker.tsx
@@ -1,17 +1,12 @@
-import SelectDropdown from "react-native-select-dropdown";
+import { useColorScheme } from "react-native";
 
-import { useRoomContext } from "../../context/room-context";
-import { StyleSheet, useColorScheme } from "react-native";
-import fonts from "../../constants/fonts";
-import colors from "../../constants/colors";
 import { Dropdown } from "../ui/dropdown";
 
+import { useRoomContext } from "../../context/room-context";
+
 export const RoomPicker = () => {
-  const colorScheme = useColorScheme();
   const { status, selectedRoom, setSelectedRoom, rooms, userRooms } =
     useRoomContext();
-
-  const palette = colors[colorScheme];
 
   if (status === "loading") {
     return null;
@@ -30,45 +25,5 @@ export const RoomPicker = () => {
       value={selectedRoom}
       onChange={setSelectedRoom}
     />
-    // <SelectDropdown
-    //   onSelect={(itemValue) => setSelectedRoom(itemValue)}
-    //   data={
-    //     rooms?.map((room) => ({
-    //       label: `${room.name} (${room.building}-${room.room})`,
-    //       value: room.id,
-    //       key: room.id,
-    //     })) ?? []
-    //   }
-    //   style={{
-    //     inputAndroid: {
-    //       fontFamily: fonts.poppinsBold,
-    //       fontSize: 16,
-    //       color: "#fff",
-    //       borderWidth: 1,
-    //       width: "100%",
-    //       backgroundColor: palette.tint,
-    //       borderRadius: 8,
-    //       padding: 8,
-    //       marginVertical: 8,
-    //     },
-    //   }}
-    //   // style={[styles.picker, { backgroundColor: palette.tint }]}
-    //   // itemStyle={styles.pickerItem}
-    // />
   );
 };
-
-const styles = StyleSheet.create({
-  picker: {
-    borderWidth: 1,
-    width: "100%",
-    fontFamily: fonts.poppinsBold,
-    fontSize: 16,
-    color: "#fff",
-  },
-  pickerItem: {
-    fontFamily: fonts.poppinsRegular,
-    fontSize: 16,
-    color: "#fff",
-  },
-});

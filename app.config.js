815f2b3f79225a9272004d809a0b2bba316c6b15
Saúl Ramos <phasecross@hotmail.com>
2023-12-26 14:15:58 -0700

    Add location permission for old devices

--- app.config.js
+++ app.config.js
@@ -31,6 +31,7 @@ export default {
         "android.permission.BLUETOOTH",
         "android.permission.BLUETOOTH_ADMIN",
         "android.permission.BLUETOOTH_CONNECT",
+        "android.permission.ACCESS_FINE_LOCATION",
       ],
       package: "com.csipro.nativeAccess",
     },

3a7dc7e44a7a2897214de3d29f54f7fc38667eb5
Saúl Ramos <phasecross@hotmail.com>
2023-12-28 00:00:10 -0700

    Set header tint color

--- app/(app)/profile/_layout.tsx
+++ app/(app)/profile/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
 
+import colors from "@/constants/colors";
+
 export default function ProfileLayout() {
-  return <Stack initialRouteName="index" />;
+  return (
+    <Stack
+      initialRouteName="index"
+      screenOptions={{
+        headerTintColor: colors.default.white[100],
+      }}
+    />
+  );
 }

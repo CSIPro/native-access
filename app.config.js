30ac3d514ef2915fab27977b33f97d1a0cb649cb
saulprl <phasecross@hotmail.com>
2023-12-29 15:10:33 -0700

    Add expo-system-ui

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default {
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
+    userInterfaceStyle: "automatic",
     experiments: {
       tsconfigPaths: true,
     },

1d1f784dc2ae37a03e3e5b0cdc57772ed55ca0e4
saulprl <phasecross@hotmail.com>
2024-01-07 10:15:25 -0700

    Update to v1.0.2

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: "CSI PRO Access",
     scheme: "native-access",
     slug: "NativeAccess",
-    version: "1.0.1",
+    version: "1.0.2",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",

644f928f68ab6b64ac7d1f7a6dc749fffb2d5c8a
Saúl Ramos <phasecross@hotmail.com>
2024-07-26 15:59:13 -0700

    Set app version to 3.0.13

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: "CSI PRO Access",
     scheme: "native-access",
     slug: "NativeAccess",
-    version: "3.0.11",
+    version: "3.0.13",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",

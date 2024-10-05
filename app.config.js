96c089497850c70bf675cdcc76eadf192bd56a73
Saúl Ramos <phasecross@hotmail.com>
2024-10-05 15:19:40 -0700

    Set to version 3.6.0

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: "CSI PRO Access",
     scheme: "native-access",
     slug: "NativeAccess",
-    version: "3.5.0",
+    version: "3.6.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",

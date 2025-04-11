0bee81c056f2e062e17ceb252a31446c2fa2ec62
Saúl Ramos <phasecross@hotmail.com>
2025-04-10 23:29:32 -0700

    bump to 3.7.3

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: "CSI PRO Access",
     scheme: "native-access",
     slug: "NativeAccess",
-    version: "3.7.2",
+    version: "3.7.3",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
